3eddb57e59fcbaf0c4bc5b986cd821541d688278
Tamir Taub <tamir.taub@klarna.com>
2017-04-20 15:09:38 +0300

    Add some css

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <ActionBar addNote={this.addNote}
                    addNoteEnabled={this.state.addNoteEnabled} />
         <NoteList notes={this.state.notes}
